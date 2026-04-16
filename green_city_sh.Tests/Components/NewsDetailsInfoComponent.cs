d637a7c78ecd086581532f5a4c8ab9d8ce304178
anqiqhr <petroderlytsia@gmail.com>
2026-04-16 23:03:41 +0300

    Updated NewsDetailsPage and its components

--- green_city_sh.Tests/Components/NewsDetailsInfoComponent.cs
+++ green_city_sh.Tests/Components/NewsDetailsInfoComponent.cs
@@ -2,13 +2,13 @@ using OpenQA.Selenium;
 
 namespace green_city_sh.Tests.Components;
 
-public class HeaderComponent: BaseComponent
+public class NewsDetailsInfoComponent: BaseComponent
 {
-    public HeaderComponent(IWebDriver driver, By rootLocator) : base(driver, rootLocator)
+    public NewsDetailsInfoComponent(IWebDriver driver, By rootLocator) : base(driver, rootLocator)
     {
     }
 
-    public HeaderComponent(IWebDriver driver, IWebElement componentRoot) : base(driver, componentRoot)
+    public NewsDetailsInfoComponent(IWebDriver driver, IWebElement componentRoot) : base(driver, componentRoot)
     {
     }
 }
---
+++ green_city_sh.Tests/Pages/NewsDetailsPage.cs
@@ -0,0 +1,12 @@
+using OpenQA.Selenium;
+using green_city_sh.Tests.Components;
+using green_city_sh.Tests.Infrastructure;
+
+namespace green_city_sh.Tests.Pages;
+
+public class NewsDetailsPage : BasePage
+{
+    public NewsDetailsPage(IWebDriver driver) : base(driver)
+    {
+    }
+}
