491ff5aae923159254f1f22034dd7ab1ab20ba72
BogdanBorsukevich <borsukevich.bogdan173@gmail.com>
2026-04-29 00:03:29 +0300

    Update CreateEventsTests

--- green_city_sh.Tests/Tests/CreateEventsTests.cs
+++ green_city_sh.Tests/Tests/CreateEventsTests.cs
@@ -8,90 +8,68 @@ using green_city_sh.Tests.Infrastructure;
 using green_city_sh.Tests.Pages;
 using green_city_sh.Tests.Components;
 
-namespace green_city_sh.Tests
+namespace green_city_sh.Tests.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Self)]
+public class CreateEventsTests : BaseTest
 {
-    [TestFixture]
-    [Parallelizable(ParallelScope.Self)]
-    public class CreateEventsTests : BaseTest
+    private CreateUpdateEventPage? createEventPage;
+
+    protected override void OnSetup()
     {
-        private CreateUpdateEventPage? createEventPage;
+        NavigateToBaseUrl();
 
-        [SetUp]
-        public void SetUp()
-        {
-            var wait = new WebDriverWait(Driver!, TimeSpan.FromSeconds(Configuration.DefaultTimeout));
+        var wait = new WebDriverWait(Driver!, TimeSpan.FromSeconds(Configuration.DefaultTimeout));
 
-            Driver!.Navigate().GoToUrl("https://www.greencity.cx.ua/#/greenCity");
+        var signInHeaderBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(), 'Sign in')]")));
+        signInHeaderBtn.Click();
 
-            var eventsTab = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='#/greenCity/events']")));
-            eventsTab.Click();
+        var signInModal = SignInModalComponent.WaitAndCreate(Driver!);
+        
+        signInModal.Login(Configuration.TestEmail, Configuration.TestPassword);
 
-            var signInHeaderBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[contains(text(), 'Sign in')]")));
-            signInHeaderBtn.Click();
+        Driver!.FindElement(By.CssSelector("a[href='#/greenCity/events']")).Click();
 
-            var signInModal = SignInModalComponent.WaitAndCreate(Driver!);
-            signInModal.Login("greencitytest69@hotmail.com", "asweQA5346!)");
+        var createEventBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[contains(., 'Create event') or contains(@class, 'create-btn')]")));
+        createEventBtn.Click();
 
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
-                    var eventsTabAfterLogin = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='#/greenCity/events']")));
-                    eventsTabAfterLogin.Click();
-                    break;
-                }
-                catch (StaleElementReferenceException) { Thread.Sleep(500); }
-            }
+        createEventPage = new CreateUpdateEventPage(Driver!);
+    }
 
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
-                    var createEventBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[contains(., 'Create event') or contains(@class, 'create-btn')]")));
-                    createEventBtn.Click();
-                    break;
-                }
-                catch (StaleElementReferenceException) { Thread.Sleep(500); }
-            }
+    [Test]
+    [Category("Smoke")]
+    public void VerifyUserCanCreateNewEvent()
+    {
+        createEventPage!.SetTitle("Green City Cleanup Festival 2026");
+        createEventPage.SelectInitiativeType("Social");
+        createEventPage.SelectEventType("Open");
+        createEventPage.SetDescription("Join us to clean the park and make our city greener!");
 
-            createEventPage = new CreateUpdateEventPage(Driver!);
-        }
+        string tomorrow = DateTime.Now.AddDays(1).ToString("MM/dd/yyyy");
+        createEventPage.SetDate(tomorrow);
+        createEventPage.SetTime("23:30", "23:59");
 
-        [Test]
-        [Category("Smoke")]
-        public void VerifyUserCanCreateNewEvent()
-        {
-            createEventPage!.SetTitle("Green City Cleanup Festival 2026");
-            createEventPage.SelectInitiativeType("Social");
-            createEventPage.SelectEventType("Open");
-            createEventPage.SetDescription("Join us to clean the park and make our city greener!");
+        createEventPage.SelectPlaceLocation();
+        createEventPage.EnterAddress("Mitskevycha Square, 14, Ivano-Frankivs'k");
+        createEventPage.SelectAddressFromDropdown();
 
-            string tomorrow = DateTime.Now.AddDays(1).ToString("MM/dd/yyyy");
-            createEventPage.SetDate(tomorrow);
-            createEventPage.SetTime("21:00", "22:00");
+        Driver!.FindElement(By.CssSelector("input[formcontrolname='place']")).Click();
+        Thread.Sleep(1000);
 
-            createEventPage.SelectPlaceLocation();
-            createEventPage.EnterAddress("Mitskevycha Square, 14, Ivano-Frankivs'k");
-            createEventPage.SelectAddressFromDropdown();
+        createEventPage.ClickPreview();
+        createEventPage.ClickBackToEditing();
+        createEventPage.ClickPublish();
 
-            // Error fix!!!
-            Driver!.FindElement(By.CssSelector("input[formcontrolname='place']")).Click();
-            Thread.Sleep(1000);
+        var wait = new WebDriverWait(Driver!, TimeSpan.FromSeconds(Configuration.DefaultTimeout));
+        wait.Until(d => d.Url.Contains("/events"));
 
-            createEventPage.ClickPreview();
-            createEventPage.ClickBackToEditing();
-            createEventPage.ClickPublish();
+        Assert.IsTrue(Driver!.Url.Contains("/events"), "Redirection failed: User was not redirected to the events page after publishing.");
+    }
 
-            var wait = new WebDriverWait(Driver!, TimeSpan.FromSeconds(10));
-            wait.Until(d => d.Url.Contains("/events"));
-
-            Assert.IsTrue(Driver!.Url.Contains("/events"), "Redirection failed");
-        }
-
-        [TearDown]
-        public void LocalTearDown()
-        {
-            // Postconditions: Delete created event
-        }
+    [TearDown]
+    public void LocalTearDown()
+    {
+        // Postconditions: Delete created event
     }
 }
