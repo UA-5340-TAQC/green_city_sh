a08e35739df1b834fa5d85d8b8f8fd29c33c89ca
anqiqhr <petroderlytsia@gmail.com>
2026-05-12 12:51:09 +0300

    Implemented api tests for eco news comment controller

---
+++ green_city_sh.Tests/Api/DTO/EcoNewsComment/AddCommentRequest.cs
@@ -0,0 +1,8 @@
+namespace green_city_sh.Tests.Api.DTO.EcoNewsComment;
+
+public class AddCommentRequest
+{
+    public string Text { get; set; }
+    
+    public long? ParentCommentId { get; set; }
+}
---
+++ green_city_sh.Tests/Api/DTO/EcoNewsComment/CommentResponse.cs
@@ -0,0 +1,22 @@
+namespace green_city_sh.Tests.Api.DTO.EcoNewsComment;
+public class EcoNewsCommentResponse
+{
+    public long Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+        
+    public Author Author { get; set; }
+        
+    public long? ParentCommentId { get; set; }
+        
+    public string Text { get; set; }
+    public int Replies { get; set; }
+    public int Likes { get; set; }
+    public int Dislikes { get; set; }
+    public bool CurrentUserLiked { get; set; }
+    public bool CurrentUserDisliked { get; set; }
+    public string Status { get; set; }
+        
+    public List<string> AdditionalImages { get; set; } 
+}
